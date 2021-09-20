cc1bc2c82a63343df24e9e61801b8a74cc62c59b
Pablo Hernández <pablohdezj03@gmail.com>
2021-09-20 13:41:22 +0100

    Refactored HadronLib.Tests to use xUnit.net

--- tests/HadronLib.Tests/Registry/Registry.cs
+++ tests/HadronLib.Tests/Registry/Registry.cs
@@ -1,27 +1,27 @@
 using System;
 using System.Linq;
 using HadronLib.Registry;
-using NUnit.Framework;
+using Xunit;
 
 namespace HadronLib.Tests.Registry
 {
     public static class Registry
     {
-        [Test]
+        [Fact]
         public static void Returns_True_If_Software_Installed()
         {
             var isUnityInstalled = RegistryTools.IsSoftwareInstalled("Microsoft", out var keyName);
             Console.WriteLine($"The key name of the software is: {keyName}");
             
-            Assert.IsTrue(isUnityInstalled);
+            Assert.True(isUnityInstalled);
         }
 
-        [Test]
+        [Fact]
         public static void Getting_Something_On_Generic_Search()
         {
             var results = RegistryTools.GetSoftwareRegistryPaths("Microsoft");
 
-            Assert.IsTrue(results.Any());
+            Assert.True(results.Any());
         }
     }
 }
