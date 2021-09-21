6380758e11b07909db5915f9b148674e8663e4e7
Pablo Hernández <pablohdezj03@gmail.com>
2021-09-21 19:17:22 +0100

    Refactored the RegistryTools and related build Tasks
    Signed-off-by: Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>

--- src/HadronLib.Tasks/IsUnityInstalledTask.cs
+++ src/HadronLib.Tasks/IsUnityInstalledTask.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using WinRegistry = Microsoft.Win32.Registry;
@@ -10,17 +11,24 @@ namespace HadronLib.Tasks
     {
         [Output]
         public String IsInstalled { get; set; }
+        
         [Output]
         public String RegistryKeyName { get; set; }
         
+        [Output]
+        public String RegistryDisplayName { get; set; }
         
         public override bool Execute()
         {
-            var isInstalled = RegistryTools.IsSoftwareInstalled("Unity 2020", out string keyName);
-
+            var isInstalled = RegistryTools.IsSoftwareInRegistry(
+                new Regex("^Unity.*"),
+                out string keyName, 
+                out var displayName);
+            
             IsInstalled = isInstalled.ToString();
             RegistryKeyName = keyName ?? "";
-
+            RegistryDisplayName = displayName ?? "";
+            
             return true;
         }
     }
